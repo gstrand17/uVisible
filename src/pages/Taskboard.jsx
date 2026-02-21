33c5c4cbd158fa5564ca9c7d6e759848d1350397
gstrand17 <gstrand@ufl.edu>
2026-02-20 23:50:11 -0500

    Taskboard rough draft

--- src/pages/Taskboard.jsx
+++ src/pages/Taskboard.jsx
@@ -1,12 +1,23 @@
 import Navbar from "../Navbar";
-import '../App.css'
+import './Taskboard.css'
 
 function Taskboard() {
     return(
         <>
             <Navbar />
-            <h1> This is my taskboard page</h1>
+            <h1 className="heading">Welcome Maria</h1>
+            <div className="member">
+                <h2 className="memberName">Mom</h2>
+                <h3 className="frequency">--daily--</h3>
+                <p className="task">Dishes</p>
+                <p className="task">Walk dog</p>
+                <p className="task">pick up kids from school</p>
+                <h3 className="frequency">--weekly--</h3>
+                <p className="task">Pick up dry cleaning</p>
+
+            </div>
         </>
     )
 }
+
 export default Taskboard;
