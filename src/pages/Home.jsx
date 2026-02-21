79b12814b893b137570e51ff0565bd553907ee55
alfitzr <alannarobbert@gmail.com>
2026-02-21 00:06:12 -0500

    Changed size of woman image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
+ls
 import React from 'react';
 import Navbar from '../Navbar.jsx';
 import Woman from '../assets/woman.png';
 import "./Home.css"
 import {Link} from "react-router-dom";
 
-
 function Home() {
     return (
         <>
-            <div class="hero">
-                <h1>Balancing the unseen tasks shouldered by women</h1>
-                <img src={Woman} alt="woman"></img>
+            <div className="hero">
+                <Header>Balancing the unseen tasks shouldered by women</Header>
+                <img src={Woman} alt="woman" className="hero-image"></img>
             </div>
             <Link to="/Login">Login</Link>
 
