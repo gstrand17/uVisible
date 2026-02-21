250f28cd26b3230bced8a852477b9dd64ac18c6e
Priya Schramm <schrammfamily@MacBook-Air-249.local>
2026-02-20 23:54:38 -0500

    add login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,30 @@
 import Navbar from "../Navbar";
-import '../App.css'
+import './Login.css'
+import {Link} from "react-router-dom";
+import viteLogo from "../assets/vite.svg";
 
 function Login() {
     return(
         <>
             <Navbar />
-            <h1> This is my log in page</h1>
+            <br />
+            <h1> Log in to uVisible</h1>
+            <form>
+                <div>                                      </div>
+                <p>Username: </p>
+                <input type='text' id='username'/>
+            </form>
+            <form>
+                <div>
+                    <p>Password: </p>
+                    <input type='text' id='password'/>
+                </div>
+            </form>
+            <div>
+                <Link to="/Signup">Sign up here!</Link>
+            </div>
         </>
     )
 }
 export default Login;
+
