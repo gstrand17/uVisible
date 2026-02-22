ed6c56fe87675e1f82d81f05c04fd9156b8c48d5
alfitzr <alannarobbert@gmail.com>
2026-02-21 20:50:46 -0500

    worked on homepage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,8 @@
 import Navbar from "../Navbar";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import './Login.css'
 import {Link, useNavigate} from "react-router-dom";
 import {supabase} from "../supabaseClient.js";
 import { useState } from "react";
-=======
-import '../App.css'
->>>>>>> f4b3a63812f20e2e18333e1761e34d3b225a7498
-=======
-import '../App.css'
->>>>>>> f4b3a63812f20e2e18333e1761e34d3b225a7498
 
 function Login() {
     const navigate = useNavigate();
@@ -41,8 +33,6 @@ function Login() {
     return(
         <>
             <Navbar />
-<<<<<<< HEAD
-<<<<<<< HEAD
             <br />
             <h1 className = "header" > Log in to uVisible</h1>
 
@@ -68,22 +58,7 @@ function Login() {
                 </form>
                 <Link className="signup" to="/Signup">Don't have a username? Sign up here!</Link>
             </div>
-=======
-            <h1> This is my log in page</h1>
->>>>>>> f4b3a63812f20e2e18333e1761e34d3b225a7498
         </>
     );
 }
-<<<<<<< HEAD
-
 export default Login;
-=======
-export default Login;
->>>>>>> f4b3a63812f20e2e18333e1761e34d3b225a7498
-=======
-            <h1> This is my log in page</h1>
-        </>
-    )
-}
-export default Login;
->>>>>>> f4b3a63812f20e2e18333e1761e34d3b225a7498
