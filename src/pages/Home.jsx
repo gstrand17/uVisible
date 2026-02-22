11f0625291937887629c5a705731000048137d08
alfitzr <alannarobbert@gmail.com>
2026-02-22 01:34:43 -0500

    correct typos in statement purpose on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
                 <div className="content-box">
                     <h2>uVisible is dedicated to making women’s work more visible.</h2>
                     <p>Invisible labor was first discussed by Arlene Kaplan Daniels in the 1980s, who described the unpaid and unseen tasks and responsibilities involved in managing a household and family. Studies have correlated these tasks to burnout, reduced time for self-care, imbalance in work-life balance, and increased pressure and expectations.</p>
-                    <p>What uVisbile aims for is making the lives of women easier through its calendar-tracking system. Family members will be able to see tasks which needed to be done, which aren’t always accounted for. Tasks include walking the dog, doing the dishes and dropping the kids off at soccer practice.</p>
+                    <p>What uVisible to do is make women's lives easier through its calendar-tracking system. Family members will be able to see tasks which needed to be done, which aren’t always accounted for. Tasks include walking the dog, doing the dishes and dropping the kids off at soccer practice.</p>
 
                     <Link to="/Login" className="login-button">Login</Link>
                 </div>
